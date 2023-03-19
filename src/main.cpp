01528de3909bcd984ac90ee7d1702ab0b3cf296e
Shramramblok <roeilustig1@gmail.com>
2023-03-19 15:19:11 +0200

    Main Files

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
